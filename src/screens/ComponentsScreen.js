ea54e188885b6c6144230bc91000d5b9ed582a63
Josh <joshwestbury@gmail.com>
2019-09-05 20:41:26 -0500

    modify component for JSX exercise

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,19 +2,20 @@ import React from "react";
 import { Text, StyleSheet, View } from "react-native";
 
 const ComponentsScreen = () => {
-  const greeting = <Text>Hello to you!</Text>;
+  const name = "Josh";
 
   return (
     <View>
-      <Text style={styles.textStyle}>This is the components screen</Text>
-      {greeting}
+      <Text style={styles.textStyle}>Getting started with React Native!</Text>
+      <Text style={styles.subHeader}>My name is {name}</Text>
     </View>
   );
 };
 const styles = StyleSheet.create({
   textStyle: {
-    fontSize: 30
-  }
+    fontSize: 45
+  },
+  subHeader: { fontSize: 20 }
 });
 
 export default ComponentsScreen;
